14582be9c336a1a0998cafb789dc702fdf412c2f
DanielOiveira <danielcompositor1307@gmail.com>
2021-08-12 18:42:12 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/danieloliveira/cursomc/services/CategoriaService.java
+++ src/main/java/com/danieloliveira/cursomc/services/CategoriaService.java
@@ -7,17 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.danieloliveira.cursomc.domain.Categoria;
 import com.danieloliveira.cursomc.repositories.CategoriaRepository;
+import com.danieloliveira.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
-	
-	public Categoria buscar(Integer id ) {
+
+	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
