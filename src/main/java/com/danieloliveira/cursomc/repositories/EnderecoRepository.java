3617d2eb4ae94222c8cee729682645642059ae45
DanielOiveira <danielcompositor1307@gmail.com>
2021-08-12 20:09:11 -0300

    Cliente tipo cliente, telefones e endereco

---
+++ src/main/java/com/danieloliveira/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.danieloliveira.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.danieloliveira.cursomc.domain.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}
