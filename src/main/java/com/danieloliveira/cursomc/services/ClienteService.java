5cfbcc4b2d3356a41b9be30d1c2103c17952d3c5
DanielOiveira <danielcompositor1307@gmail.com>
2021-08-12 20:36:41 -0300

    Endpoint/clientes/{id} disponivel

---
+++ src/main/java/com/danieloliveira/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.danieloliveira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danieloliveira.cursomc.domain.Cliente;
+import com.danieloliveira.cursomc.repositories.ClienteRepository;
+import com.danieloliveira.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj  = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Cliente não encontrado" + Cliente.class.getName())); 
+	} 
+	
+
+}
