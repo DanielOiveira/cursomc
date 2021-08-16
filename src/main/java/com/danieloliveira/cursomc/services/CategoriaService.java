ce8e5efb0f94c1e729a440b2118df658d2102cbf
DanielOiveira <danielcompositor1307@gmail.com>
2021-08-16 06:50:32 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/danieloliveira/cursomc/services/CategoriaService.java
+++ src/main/java/com/danieloliveira/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.danieloliveira.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +43,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que inclui produtos");
 		}
 		
+	}
+	
+	public List<Categoria> findAll(){
+		return repository.findAll();
 	}
 
 }
