22ebd2e6d4cda4840c41cfe22b9b1f528ec40faf
DanielOiveira <danielcompositor1307@gmail.com>
2021-09-22 21:54:18 -0300

    Ajustes para retornar o id do pedido de forma correta

--- src/main/java/com/danieloliveira/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/danieloliveira/cursomc/repositories/PedidoRepository.java
@@ -5,11 +5,13 @@ package com.danieloliveira.cursomc.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.danieloliveira.cursomc.domain.Cliente;
 import com.danieloliveira.cursomc.domain.Pedido;
 
+@Repository
 public interface PedidoRepository  extends JpaRepository<Pedido, Integer>{
 
 	@Transactional(readOnly = true)
--- src/main/java/com/danieloliveira/cursomc/resources/PedidoResource.java
+++ src/main/java/com/danieloliveira/cursomc/resources/PedidoResource.java
@@ -32,13 +32,13 @@ public class PedidoResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
-		 obj = service.insert(obj);
-		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				 .path("{/id}").buildAndExpand(obj.getId()).toUri();
-		 return ResponseEntity.created(uri).build();
-	} 
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<Page<Pedido>> findPage(
